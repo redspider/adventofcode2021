c320f5f32f4a64dde53e5620b2d7f425feb8f4f3
Richard Clark <richard@redspider.co.nz>
2021-12-22 09:42:04 +1300

    Did thinking :(

--- gus_special.py
+++ gus_special.py
@@ -1,13 +1,22 @@
-people = list(range(1, 101))
+def solve(n):
+    people = list(range(1, n + 1))
 
-while len(people) > 1:
-    people = people[2:] + [people[0]]
+    while len(people) > 1:
+        people = people[2:] + [people[0]]
 
-print("Part 1:", people)
+    return people[0]
 
-people = list(range(1, 1547))
 
-while len(people) > 1:
-    people = people[2:] + [people[0]]
+def solve2(n):
+    x = 2
+    ox = 2
+    while x < n + 1:
+        ox = x
+        x = x * 2
+    return (n - ox) * 2 + 1
 
-print("Part 2:", people)
+
+for n in range(2, 100):
+    print(n, solve(n), solve2(n))
+
+print(solve2(30_000_000_000))
